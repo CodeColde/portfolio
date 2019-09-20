1f1b1a0275092f483cafff5fe39c68febe72fad8
codecolde <hayo.web@gmail.com>
2019-09-21 01:07:20 +0200

    initial commit

---
+++ src/pages/cases/Adscope.js
@@ -0,0 +1,279 @@
+import React, { Component, createRef, Fragment } from "react";
+import {
+  Hero,
+  TitleTab,
+  Title,
+  TechniqueTitle,
+  Introduction,
+  InfoTitle,
+  InfoText,
+  QuickInfo,
+  InfoClient,
+  Paragraph,
+  InfoList,
+  InfoListContainer,
+  InfoItem,
+  InfoSubject,
+  InfoContent,
+  InfoSubtitle,
+  ClientHeader,
+  CaseWrapper,
+  Problem,
+  Process,
+  Results,
+  NextCase,
+  Subheading,
+  TinyHeading,
+  CaseImagesCenter,
+  CaseImagesLeft,
+  NextLink,
+  NextCaseTitle,
+  BackButton,
+  OrderedList,
+  BackArrow
+} from "../../styles/Case.styles";
+import { withRouter } from "react-router";
+
+// Has a scroll function to detect if the body is a black background and therefore
+// Has to switch the backbutton to a black background
+
+class Adscope extends Component {
+  constructor(props) {
+    super(props);
+    this.introRef = createRef();
+
+    this.state = {
+      toBack: false,
+      introTop: 0,
+      hasBackground: false,
+      animateCase: ""
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+    this.setState({
+      introTop: this.introRef.current.getBoundingClientRect().top - 40
+    });
+  }
+
+  componentDidUpdate() {
+    if (this.state.toBack) {
+      setTimeout(() => {
+        this.props.setNavBackground(false);
+        this.props.history.push("/work");
+      }, 400);
+    }
+    if (this.state.animateCase) {
+      setTimeout(() => {
+        this.props.setNavBackground(false);
+
+        this.props.history.push(this.state.animateCase);
+        window.scrollTo(0, 0);
+      }, 400);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.toBack) {
+      window.scrollTo(0, 0);
+    }
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const introTop = this.introRef.current.getBoundingClientRect().top;
+    if (introTop < 40) {
+      this.setState({
+        hasBackground: true
+      });
+      this.props.setNavBackground(true);
+    } else {
+      this.setState({
+        hasBackground: false
+      });
+      this.props.setNavBackground(false);
+    }
+  };
+
+  render() {
+    const hero = require("../../assets/cases/case-2-adscope/adscope-hero.jpg");
+    const backButton = require("../../assets/icons/back.png");
+    return (
+      <Fragment>
+        <BackButton
+          onClick={() => this.setState({ toBack: true, hasBackground: false })}
+          toBack={this.state.toBack}
+          hasBackground={this.state.hasBackground}
+        >
+          <BackArrow src={backButton} alt="Back to work" />
+        </BackButton>
+        <CaseWrapper>
+          <Hero image={hero}>
+            <div>
+              <TitleTab>
+                <ClientHeader>Northumbria University</ClientHeader>
+                <Title>Adscope</Title>
+                <TechniqueTitle>
+                Micromanaging, transparency, efficiency, output; with a world so busy, organization has priority above all else for smooth operations
+                </TechniqueTitle>
+              </TitleTab>
+            </div>
+          </Hero>
+          <Introduction ref={this.introRef}>
+            <QuickInfo>
+              <InfoText>
+                <InfoClient>Northumbria University</InfoClient>
+                <InfoTitle>Adscope</InfoTitle>
+                <InfoSubtitle>
+                  Micromanaging, transparency, efficiency, output; with a world so busy, organization has priority above all else for smooth operations
+                </InfoSubtitle>
+                <Paragraph>
+                  For my dissertation I took a deeper dive into the complex nature behind advertising industries, inspired by my two terms at J. Walter Thompson Amsterdam. The goal was to create a system that illustrated the research I delved into regarding some real world issue the advertising industry has, and to evaluate how user experience can tackle certain problems they may have.
+                </Paragraph>
+              </InfoText>
+              <InfoListContainer>
+                <InfoList>
+                  <InfoItem>
+                    <InfoSubject>Project Client</InfoSubject>
+                    <InfoContent>Northumbria University</InfoContent>
+                  </InfoItem>
+                  <InfoItem>
+                    <InfoSubject>Focus</InfoSubject>
+                    <InfoContent>UX / Back-End</InfoContent>
+                  </InfoItem>
+                  <InfoItem>
+                    <InfoSubject>Date</InfoSubject>
+                    <InfoContent>April 2017</InfoContent>
+                  </InfoItem>
+                </InfoList>
+              </InfoListContainer>
+            </QuickInfo>
+            <Problem>
+              <Subheading>Problem</Subheading>
+              <Paragraph>
+                My research found three core problems the advertising industry had that this project would hammer into. These were:
+              </Paragraph>
+              <OrderedList>
+                <li>Lack of Transparency</li>
+                <li>Maximizing Productivity</li>
+                <li>Lasting Clientele</li>
+              </OrderedList>
+              <Paragraph>
+                The lack of transparency is a frequent complain clients have with advertising agencies, though this seems to tackle the financial aspect the most: What is it we're paying for?
+              </Paragraph>
+              <Paragraph>
+                Maximizing productivity is the goal for any agency. Each has their own method that works best, applying elements like scrum or waterfall productivity methods or structuring their workforce a certain way, to name a few. To see this occur, one would want to tackle the ability to track their workforce. 
+              </Paragraph>
+              <Paragraph>
+              	For a consistent revenue stream, lasting clientele creates definite business. But most clients tend to leave after a campaign is over or when they found another. This problem ties into most of the others; efficient productivity and complete transparency are some of the things that clients love. How does an agency keep a client.
+              </Paragraph>
+            </Problem>
+          </Introduction>
+          <Process>
+            <Subheading>Process</Subheading>
+            <TinyHeading>Branding</TinyHeading>
+            <Paragraph>
+              The platform is in its essence focused on the data, clients, and the content, and rather than applying Adscope's own brand image to it, the base color of white with a reddish accent creates a company's own interest in taking part in the platform without making use of a heavily branded platform. The font is also legible and simplistic. Below the full guide.
+            </Paragraph>
+            <CaseImagesLeft>
+              <img src={require("../../assets/cases/case-2-adscope/asbranding.png")} alt="branding" />
+            </CaseImagesLeft>
+            <Paragraph>
+              The development of the prototype of the productivity platform was already seeming too large, and so the project was cut down to the bare minimum to tackle the three important features. The product had the following core features.
+            </Paragraph>
+            <TinyHeading>Communication</TinyHeading>
+            <Paragraph>
+              The project had several different methods to communicate. To set it up, I created a contact system that showed client information, colleague information, or resource information for quick easy access. This information is granted to all or selected colleagues on the platform.
+            </Paragraph>
+            <CaseImagesCenter>
+              <img src={require("../../assets/cases/case-2-adscope/addressbook.png")} alt="Address Book" />
+              <img src={require("../../assets/cases/case-2-adscope/contactcard.png")} alt="Contact Card" />
+            </CaseImagesCenter>
+            <Paragraph>
+              The card and user system is applied to the rest of the system as well.
+            </Paragraph>
+            <TinyHeading>Projects &amp; Clients</TinyHeading>
+            <Paragraph>
+              Creating a hub for the project and a hub for new/existing clients is what allows the system to organize everything. In order to practice transparency, the system allows anything to be logged on the basis of who's involved in the process, and hence a company gets full insight as to who attended and worked on what, if they want to see the breakdown of the costs.
+            </Paragraph>
+            <CaseImagesLeft>
+              <img src={require("../../assets/cases/case-2-adscope/ashome.png")} alt="Home Page" />
+            </CaseImagesLeft>
+            <Paragraph>
+              Clicking on a project will link the user to the first image below. Clicking on a client would like them to the second. The hubs look the same, but one is divided for client information and the other for a project.
+            </Paragraph>
+            <CaseImagesLeft>
+              <img src={require("../../assets/cases/case-2-adscope/asprojects.png")} alt="Projects List" />
+            </CaseImagesLeft>
+            <CaseImagesLeft>
+              <img src={require("../../assets/cases/case-2-adscope/asclients.png")} alt="Clients List" />
+            </CaseImagesLeft>
+            <Paragraph>
+              Each project has the following functionality that can be ammended at any time. The clients have less, but similar features.
+            </Paragraph>
+            <CaseImagesCenter>
+              <img src={require("../../assets/cases/case-2-adscope/project1.png")} alt="Projects One" />
+              <img src={require("../../assets/cases/case-2-adscope/project2.png")} alt="Projects Two" />
+            </CaseImagesCenter>
+            <CaseImagesCenter>
+              <img src={require("../../assets/cases/case-2-adscope/project3.png")} alt="Projects Three" />
+              <img src={require("../../assets/cases/case-2-adscope/project4.png")} alt="Projects Four" />
+            </CaseImagesCenter>
+            <TinyHeading>Hour Logging</TinyHeading>
+            <Paragraph>
+              The most important feature in a project hub is the tasks section. This is where a project manager can delegate tasks to colleagues, and this will allow colleagues to work on them and track them in their own available time. Tracking the time that colleagues work on a project is key to transparent billing. There are 2 ways they log their hours. The first is through events:
+            </Paragraph>
+            <CaseImagesLeft>
+              <img src={require("../../assets/cases/case-2-adscope/events.png")} alt="Events" />
+            </CaseImagesLeft>
+            <Paragraph>
+              By setting up a calendar event, either for meetings or a scheduled workblock, either alone or with others, the user creates a timeslot that delegates their salary and calculates the amount of time human resources are needed to be put to work in a given time period. These events will be placed on the invoice for more detail to the client, but will omit the individuals involved, as well as their salaries, for privacy. One feature missing here is to toggle between billable and non-billable, such as for time spent on brainstorming a project to pitch to a client, for example.
+            </Paragraph>
+            <Paragraph>
+              The second way one can log their hours is via their tasks, and a built-in timer that runs passively.
+            </Paragraph>
+            <CaseImagesLeft>
+              <img src={require("../../assets/cases/case-2-adscope/timer.png")} alt="Timer" />
+            </CaseImagesLeft>
+            <TinyHeading>Invoice</TinyHeading>
+            <Paragraph>
+              A generated invoice would look something like below:
+            </Paragraph>
+            <CaseImagesCenter>
+              <img src={require("../../assets/cases/case-2-adscope/invoice.png")} alt="Invoice" />
+            </CaseImagesCenter>
+            <Paragraph>
+              As stated in the previous section, the invoice is currently missing details that are equally important, but was ommitted to convey the basics within the available timeframe.
+            </Paragraph>
+          </Process>
+          <Results>
+            <Subheading>Result</Subheading>
+            <Paragraph>
+              As I had recently come out of a job experience working as a project management intern, I took this opportunity to get to know the industry better through this project. Although that goal was accomplished, there was a lot more I wanted to do with this program.
+            </Paragraph>
+            <Paragraph>
+              The program feels incredibly incomplete, and despite the available functionality working well, and user tests proving users learn to use the system quickly, there are still some UX flaws, mainly surrounding generating project invoices, that need fixing, and a lot of functionality that needs implementing. 
+            </Paragraph>
+            <Paragraph>
+              Additionally, after doing more research, a framework named Laravel would have solved a ton of development issues I came across and had to fix, which decreased the amount of time I had available. It sparked the need to prepare more, launching my own catalogue of frameworks and what they do, to allow me to be more prepared in the future.
+            </Paragraph>
+            <Paragraph>
+              Despite the clear flaws, the project was a massive learning experience, and the generation of the invoice and the system as it stands at this point works, including the timer and the calendar, meshing alongside key components within the system.
+            </Paragraph>
+          </Results>
+          <NextCase
+            onClick={() => this.setState({ animateCase: "/tiptopmusic" })}
+            animating={!!this.state.animateCase}
+          >
+            <NextLink>
+              Next: <NextCaseTitle>Tip Top Music</NextCaseTitle>
+            </NextLink>
+          </NextCase>
+        </CaseWrapper>
+      </Fragment>
+    );
+  }
+}
+
+export default withRouter(Adscope);
---
+++ src/pages/cases/TiptopMusic.js
@@ -0,0 +1,224 @@
+import React, { Component, createRef, Fragment } from "react";
+import {
+  Hero,
+  TitleTab,
+  Title,
+  TechniqueTitle,
+  Introduction,
+  InfoTitle,
+  InfoText,
+  QuickInfo,
+  InfoClient,
+  Paragraph,
+  InfoList,
+  InfoListContainer,
+  InfoItem,
+  InfoSubject,
+  InfoContent,
+  InfoSubtitle,
+  ClientHeader,
+  CaseWrapper,
+  Problem,
+  Process,
+  Results,
+  NextCase,
+  Anchor,
+  Subheading,
+  TinyHeading,
+  CaseImagesCenter,
+  NextLink,
+  NextCaseTitle,
+  BackButton,
+  BackArrow,
+  CaseImageCenterLarge
+} from "../../styles/Case.styles";
+import { withRouter } from "react-router";
+
+// Has a scroll function to detect if the body is a black background and therefore
+// Has to switch the backbutton to a black background
+
+class TiptopMusic extends Component {
+  constructor(props) {
+    super(props);
+    this.introRef = createRef();
+
+    this.state = {
+      toBack: false,
+      introTop: 0,
+      hasBackground: false,
+      animateCase: ""
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+    this.setState({
+      introTop: this.introRef.current.getBoundingClientRect().top - 40
+    });
+  }
+
+  componentDidUpdate() {
+    if (this.state.toBack) {
+      setTimeout(() => {
+        this.props.setNavBackground(false);
+        this.props.history.push("/work");
+      }, 400);
+    }
+    if (this.state.animateCase) {
+      setTimeout(() => {
+        this.props.setNavBackground(false);
+
+        this.props.history.push(this.state.animateCase);
+        window.scrollTo(0, 0);
+      }, 400);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.toBack) {
+      window.scrollTo(0, 0);
+    }
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const introTop = this.introRef.current.getBoundingClientRect().top;
+    if (introTop < 40) {
+      this.setState({
+        hasBackground: true
+      });
+      this.props.setNavBackground(true);
+    } else {
+      this.setState({
+        hasBackground: false
+      });
+      this.props.setNavBackground(false);
+    }
+  };
+
+  render() {
+    const hero = require("../../assets/cases/case-3-tiptopmusic/tiptopmusic-hero.jpg");
+    const backButton = require("../../assets/icons/back.png");
+    return (
+      <Fragment>
+        <BackButton
+          onClick={() => this.setState({ toBack: true, hasBackground: false })}
+          toBack={this.state.toBack}
+          hasBackground={this.state.hasBackground}
+        >
+          <BackArrow src={backButton} alt="Back to work" />
+        </BackButton>
+        <CaseWrapper>
+          <Hero image={hero}>
+            <div>
+              <TitleTab>
+                <ClientHeader>Northumbria University</ClientHeader>
+                <Title>Tip Top Music</Title>
+                <TechniqueTitle>
+                  North Eastern Indie Bands don't get the attention they deserve. It's time to bring you the best that the North East has to offer
+                </TechniqueTitle>
+              </TitleTab>
+            </div>
+          </Hero>
+          <Introduction ref={this.introRef}>
+            <QuickInfo>
+              <InfoText>
+                <InfoClient>Northumbria University</InfoClient>
+                <InfoTitle>Tip Top Music</InfoTitle>
+                <InfoSubtitle>
+                  North Eastern Indie Bands don't get the attention they deserve. It's time to bring you the best that the North East has to offer
+                </InfoSubtitle>
+                <Paragraph>
+                As part of any website for consumers, API plays a vital role in developing the user experience, facilitating certain functions, and accessing certain information. API exists to provide information that already exists, and the providers of this API are specialized in maintaining its security, data, and functionalities. This assignment for Northumbria University aimed to experiment with that.
+                </Paragraph>
+              </InfoText>
+              <InfoListContainer>
+                <InfoList>
+                  <InfoItem>
+                    <InfoSubject>Project Client</InfoSubject>
+                    <InfoContent>Northumbria University</InfoContent>
+                  </InfoItem>
+                  <InfoItem>
+                    <InfoSubject>Focus</InfoSubject>
+                    <InfoContent>Front-end</InfoContent>
+                  </InfoItem>
+                  <InfoItem>
+                    <InfoSubject>Date</InfoSubject>
+                    <InfoContent>May 2017</InfoContent>
+                  </InfoItem>
+                </InfoList>
+              </InfoListContainer>
+            </QuickInfo>
+            <Problem>
+              <Subheading>Problem</Subheading>
+              <Paragraph>
+                As part of the brief, the task was to build a platform for visitors to get to know information of a North Eastern indie scene in the UK. This meant creating a platform with the ability to add more information to it in the future.
+              </Paragraph>
+              <Paragraph>
+                The track I went with is to go in depth surrounding what such a site could look like, while developing the functionality necessary for the passing mark. The site would include the ability to go to events, get information and community buzz around certain bands, and to offer them more involvement in their music scene, as well as information about the magazing and the ability to support it.
+              </Paragraph>
+              <Paragraph>
+              The main struggle with this assignment was the inexperience surrounding API's, and thus it was nervewrecking to understand the potential power an API can have, and some technological restraints. Additionally, design-wise the platform would draw inspiration from other sources, but I wanted to experiment with a different colorset I haven't seen often yet, finally dabbling in the color green after investing time in understanding the effects of red and yellow earlier in the year.
+              </Paragraph>
+            </Problem>
+          </Introduction>
+          <Process>
+            <Subheading>Process</Subheading>
+            <TinyHeading>Designs</TinyHeading>
+            <Paragraph>
+              The designs were initially experimenting with a shade of dark/light green as the typography accents, with the background accents being some shades of beige. The goal was to create a welcoming sensation.
+            </Paragraph>
+            <CaseImageCenterLarge>
+              <img src={require("../../assets/cases/case-3-tiptopmusic/Landing.png")} alt="Landing" />
+            </CaseImageCenterLarge>
+            <Paragraph>
+              As seen on the landing page, the beige was instead used as an accent to break apart sections of information, a rule which I would conform the rest of the site to as well. During the development of the design however, I became attracted to separating the content with a darker shade as the background accent accompanied by white text. This would maintain the branding guide below, with minimal use of colors while drawing the user to the information.
+            </Paragraph>
+            <CaseImageCenterLarge>
+              <img src={require("../../assets/cases/case-3-tiptopmusic/Branding.png")} alt="Branding" />
+            </CaseImageCenterLarge>
+            <Paragraph>
+              The above result was strictly adhered to for the remainder of the site, throughout its available functionality as adhered to by the assignment brief.
+            </Paragraph>
+            <TinyHeading>Development</TinyHeading>
+            <Paragraph>
+              The development saw it easy to adhere to the guidelines the branding document sought to set, and thus the front end development side was very simple to do.
+            </Paragraph>
+            <Paragraph>
+              The issues started to appear when the band pages saw the results provided by the API and the code adhering to it. For the twitter feed, I wanted to showcase a twitter-like feel, and had to hardcode the design of a twitter tag for each individual's tweet. Additionally, the band Girl Band saw a lot of profanity and pornographic tweets, and thus had to experiment with the API's logic to mitigate that.
+            </Paragraph>
+            <Paragraph>
+              The Google Maps API was by far the easiest to manage, the only desire I had being to make custom markers to identify gig locations. The design was very simple, but adhered to the color scheme.
+            </Paragraph>
+            <CaseImagesCenter>
+              <img src={require("../../assets/cases/case-3-tiptopmusic/gmicon.png")} alt="Icon" />
+            </CaseImagesCenter>
+          </Process>
+          <Results>
+            <Subheading>Result</Subheading>
+            <Paragraph>
+              Despite some API hardcoding issues and the fact that the twitter account associated with the site has been deleted, the result adhered to the site nicely. The green color scheme I experimented with showed a nice design, but whether it fit well with the indie scene would require a survey to garner results. 
+            </Paragraph>
+            <Paragraph>
+              The user experience of the site, if it would go into full development, shows potential to work seemlessly, and harmony between text and other content would go well with the site's overall design. 
+            </Paragraph>
+            <Paragraph>
+              I plan to use this style of design for a magazine site I'm currenty starting development on. Stay tuned to see that site's design.
+            </Paragraph>
+            <Anchor href="http://ec2-34-214-154-202.us-west-2.compute.amazonaws.com/Final/index.php">View Live Here</Anchor>
+          </Results>
+          <NextCase
+            onClick={() => this.setState({ animateCase: "/schoolforjustice" })}
+            animating={!!this.state.animateCase}
+          >
+            <NextLink>
+              Next: <NextCaseTitle>School For Justice</NextCaseTitle>
+            </NextLink>
+          </NextCase>
+        </CaseWrapper>
+      </Fragment>
+    );
+  }
+}
+
+export default withRouter(TiptopMusic);
