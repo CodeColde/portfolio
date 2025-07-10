c33800560d53b34f31003daf7ed8b7c01aa6ed67
CodeColde <hayo.web@gmail.com>
2025-07-10 02:57:05 +0200

    Updating the Sanity config to make the interface read something more personalized

--- sanity.config.ts
+++ sanity.config.ts
@@ -14,6 +14,7 @@ import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
 export default defineConfig({
+  title: "Hayo's Portfolio",
   basePath: '/admin',
   projectId,
   dataset,
