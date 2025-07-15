cb4996168f78bc4f6fdcaa9f22441d88387f2069
CodeColde <hayo.web@gmail.com>
2025-07-16 00:54:40 +0200

    Added parallax scroll to the home page

--- app/components/CaseSummaryItem.tsx
+++ app/components/CaseSummaryItem.tsx
@@ -2,6 +2,9 @@
 import type { CaseIntroEntry } from "../types/cases.types";
 import { urlFor } from "@/sanity/lib/image";
 import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
 import CaseLink from "./CaseLink";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -12,10 +15,36 @@ interface Props {
 }
 
 const CaseSummaryItem = ({ caseDetails, idx }: Props) => {
+  const bgRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      if (!bgRef.current) {
+        return;
+      }
+
+      gsap.fromTo(
+        bgRef.current,
+        { y: 0},
+        {
+        y: () => -window.innerHeight * 0.3,
+        ease: "none",
+        scrollTrigger: {
+          trigger: bgRef.current.parentElement,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      });
+    }, bgRef)
+
+    return () => ctx.revert();
+  }, []);
 
   return (
     <section className={`relative ${idx === 0 ? "h-[90vh]" : "h-[800px]"} w-full overflow-hidden bg-gray-500`}>
       <div
+        ref={bgRef}
         className={`absolute top-0 left-0 will-change-transform bg-cover bg-center bg-grey-800 opacity-90 w-full ${
           idx === 0
           ? "h-[150%] max-md:h-[140%] max-sm:h-[139%]"
