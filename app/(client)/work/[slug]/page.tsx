20c0de9c7c50967d2e7a21599da4794d375b1a89
CodeColde <hayo.web@gmail.com>
2025-08-08 01:05:15 +0200

    Removed useless console log

--- app/(client)/work/[slug]/page.tsx
+++ app/(client)/work/[slug]/page.tsx
@@ -23,7 +23,6 @@ const page = async ({ params }: PageProps) => {
   const { slug } = await params;
 
   const caseDetails = await getCaseBySlug(slug);
-  console.log(caseDetails);
 
   return (
     <main className="relative h-auto w-full pb-28 opacity-100">
